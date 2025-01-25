762e43cfd656a2ea9ca64222d726fc5a78063cbe
Mangochicken <97166753+Mangochicken13@users.noreply.github.com>
2025-01-25 21:20:37 +0800

    Update Breakable walls
    Now using a gradient to determine the health indicator colour

--- you-are-the-weapon/Components/Walls/breakable_wall.gd
+++ you-are-the-weapon/Components/Walls/breakable_wall.gd
@@ -7,30 +7,44 @@ var max_health: float
 @export var defence: float = 0
 @export var damage_reduction: float = 0
 
+@export var health_gradient: Gradient
 @export var crack_noise: Resource
 
-var max_health_color: Color = Color.WEB_GREEN
-var min_health_color: Color = Color.DARK_RED
 
 func _ready() -> void:
 	super._ready()
 	max_health = health
 	# Breakable Walls
 	collision_layer = 4
+	_update_color()
 	
 
-func _damage(incoming_damage: float) -> float:
+## Returns the final damage dealt after damage reduction and defence
+func _deal_damage(incoming_damage: float) -> float:
+	
 	var final_damage = incoming_damage * (1 - damage_reduction)
 	final_damage = final_damage - defence
+	
 	health = health - final_damage
+	
 	_update_cracks()
+	if health <= 0:
+		_destroy()
+	
 	_update_color()
+	
 	return final_damage
 
 func _update_color() -> void:
-	mesh_material.set("albedo_color", lerp(max_health_color, min_health_color, health/max_health))
+	# there has to be a better method to change the color of the texture right?
+	mesh_material.set("albedo_color", health_gradient.sample(1 - health/max_health))
 
 
 func _update_cracks() -> void:
 	# call function to make crack/debri particles
+	pass
+
+func _destroy() -> void:
+	queue_free()
+	#shatter into pieces, still bounce off of the last hit though
 	pass
