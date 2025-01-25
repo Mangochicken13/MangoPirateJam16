6ddd1a35f154f2ab5e28e61dd9917822fdc1ecf3
Mangochicken <97166753+Mangochicken13@users.noreply.github.com>
2025-01-25 21:21:35 +0800

    Update ball.gd

--- you-are-the-weapon/Components/ball.gd
+++ you-are-the-weapon/Components/ball.gd
@@ -3,55 +3,109 @@ class_name Ball
 
 @export var mesh: MeshInstance3D
 @onready var ball_cam: PhantomCamera3D
+@onready var ball_cam_base_spring_length: float
+var target_spring_length: float
 
 @export var speed: float = 20
 @export var turn_speed: float = 1
 
 var damage_mult: float = 1
 
-const LERP_SPEED: float = 2
+const CAMERA_LERP_SPEED: float = 2
+const SPRING_LERP_SPEED: float = 4
+const VELOCITY_LERP_SPEED: float = 1
+const ADDITIONAL_COLLISIONS: int = 3
+
 
 func _ready() -> void:
 	ball_cam = %BallCamera
+	ball_cam_base_spring_length = ball_cam.spring_length
+	target_spring_length = ball_cam_base_spring_length
 	
 	# This line is required to make the controls work as expected before hitting a wall
 	# For some reason they are inverted without this, could be a sign that something else is benig done wrong
 	# This works for now though
 	basis = Basis.looking_at(basis * speed * Vector3.FORWARD)
+	
+	# TODO: Move this function out of here once a menu is implemented; 
+	#   Call it from there instead
+	start_moving()
 
 
 func _physics_process(delta: float) -> void:
-	
+	#region Rotation
 	var turn_dir_x = Input.get_axis("turn_up", "turn_down")
 	rotate_object_local(Vector3.MODEL_RIGHT, turn_dir_x * delta)
+	velocity = velocity.rotated(basis * Vector3.LEFT, turn_dir_x * delta)
 	
 	var turn_dir_y = Input.get_axis("turn_right", "turn_left")
 	rotate_object_local(Vector3.MODEL_TOP, turn_dir_y * delta)
+	velocity = velocity.rotated(basis * Vector3.MODEL_TOP, turn_dir_y * delta)
+	#endregion
 	
-	# Constant velocity for now, change this to allow deceleration if decided as the lose condition
+	# Change this to allow deceleration if decided as the lose condition
 	# Other lose condition options include a timer or a combo/hype system
-	velocity = basis * speed * Vector3.FORWARD
+	#if velocity.length() < 20:
+		#velocity = basis * speed * Vector3.FORWARD
+	#else:
+	velocity = velocity.normalized() * lerp(velocity.length(), speed, VELOCITY_LERP_SPEED * delta)
 	
-	var collision = move_and_collide(velocity * delta)
-	# Bounce function: Needs to be moved to a seperate function call to handle different wall types
-	if collision:
-		if collision.get_collider() is Wall:
-			var normal = collision.get_normal()
-			velocity = velocity.bounce(normal)
-			
-			# No need to multiply by delta, the remainder magnitude is already multiplied by it
-			move_and_collide(velocity.normalized() * collision.get_remainder().length())
-			basis = Basis.looking_at(velocity)
-			
+	#region Collision Handling
+	var collision := move_and_collide(velocity * delta)
+	
+	for i in ADDITIONAL_COLLISIONS:
+		if collision == null:
+			break
 		
+		collision = handle_collision(collision)
+	#endregion
 	
+
+func handle_collision(last_collision: KinematicCollision3D) -> KinematicCollision3D:
+	if !last_collision:
+		return null
+	
+	var collider = last_collision.get_collider()
+	
+	if collider is BreakableWall:
+		collider._deal_damage(calculate_damage())
+		pass
+	
+	# Common logic; bouncing, imparted velocity
+	if collider is Wall:
+		
+		var normal = last_collision.get_normal()
+		velocity = velocity.bounce(normal)
+		velocity = collider._bounce(velocity)
+		
+		# No need to multiply by delta, the remainder magnitude is already multiplied by it
+		var collision = move_and_collide(velocity.normalized() * last_collision.get_remainder().length())
+		basis = Basis.looking_at(velocity)
+		return collision
+	
+	return null
+
+func calculate_damage() -> float:
+	var damage = max(0.5, (1 + pow(max(0, (velocity.length() - speed)) ** 2, (1.0/3)))) * damage_mult
+	
+	print("Damage: ", damage)
+	print("From values: \nVelocity magnitude: {0}\nSpeed: {1}\nDamage Multiplier: {2}".format([velocity.length(), speed, damage_mult]))
+	return damage
+	
+
+func start_moving() -> void:
+	velocity = basis * speed * Vector3.FORWARD
 
 func _process(delta: float) -> void:
 	# Using a lerp here to change the angle of the phantom camera spring arm 
 	#   avoids manually changing the angle along with player input, and handles bouncing
 	var ball_cam_angle = ball_cam.get_third_person_rotation()
 	for i in range(3):
-		ball_cam_angle[i] = lerp_angle(ball_cam_angle[i], global_rotation[i], LERP_SPEED * delta)
+		ball_cam_angle[i] = lerp_angle(ball_cam_angle[i], global_rotation[i], CAMERA_LERP_SPEED * delta)
 	ball_cam.set_third_person_rotation(ball_cam_angle)
+	
+	# TODO: more work needed on this
+	target_spring_length = max(ball_cam_base_spring_length, ball_cam_base_spring_length + min(2, sqrt((velocity.length() + 1 - speed) - 1)))
+	ball_cam.spring_length = lerp(ball_cam.spring_length, target_spring_length, SPRING_LERP_SPEED * delta)
 	#pcam.set_third_person_rotation(lerp(pcam.get_third_person_rotation(), global_rotation, 0.06))
 	
