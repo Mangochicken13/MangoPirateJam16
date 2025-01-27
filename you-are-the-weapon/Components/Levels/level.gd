dd2b3918180a0459b41eb4a0c25544fa11c67645
Mangochicken <97166753+Mangochicken13@users.noreply.github.com>
2025-01-27 18:49:31 +0800

    Documenting level export vars

--- you-are-the-weapon/Components/Levels/level.gd
+++ you-are-the-weapon/Components/Levels/level.gd
@@ -3,11 +3,11 @@ extends Node3D
 class_name Level
 
 
-enum WIN_CONDITION {
-	None,
-	Brick_percentage,
-	Brick_num,
-	Triggers,
+enum WIN_CONDITION { ## The condition to meet for the exit to enable
+	None, ## No condition
+	Brick_percentage, ## Break [member brick_percentage] of bricks in the level
+	Brick_num, ## Break [member brick_num] bricks in the level (must be less than or equal to the total number of bricks)
+	Triggers, ## Enter all of the areas in [member triggers]
 }
 
 @export var win_condition: WIN_CONDITION:
@@ -19,14 +19,20 @@ enum WIN_CONDITION {
 	set(num):
 		# validate this against the number of bricks in the level
 		brick_num = num
+
 @export var triggers: Array[Node3D] = []
 
-## If the selected win condition has a time to complete it in
+## If the selected win condition has a time to complete it within.
+## Using this in conjunction with [enum WIN_CONDITION.None] 
+##will give the player [member time] seconds to reach the exit
 @export var timed: bool = false:
 	set(value):
 		timed = value
 		notify_property_list_changed()
-@export var stop_timer_on_win: bool = false
+## Whether to stop counting down the timer once the condition is met.
+## Setting this to [code]false[/code] means the player has to reach the exit after completing 
+## This does nothing if the condition is [enum WIN_CONDITION.None]
+@export var stop_timer_on_win: bool = true
 @export var time: float = 120
 
 
@@ -36,6 +42,8 @@ enum WIN_CONDITION {
 func _ready() -> void:
 	if exit_trigger:
 		exit_trigger.body_entered.connect(try_finish_level)
+	else:
+		push_error("No exit trigger selected")
 
 func try_finish_level():
 	pass
