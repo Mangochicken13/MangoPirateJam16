aa2cafb407dd4761c65ef01ac96e4ab0557dd6a0
Mangochicken <97166753+Mangochicken13@users.noreply.github.com>
2025-01-31 19:39:48 +0800

    add scoring to player ui

--- you-are-the-weapon/UI/player_ui.gd
+++ you-are-the-weapon/UI/player_ui.gd
@@ -7,10 +7,10 @@ var current_level: Level
 @export_category("Internals")
 @export var score_lerp_timer: Timer
 @export var total_score_label: Label
-var total_score: int
+var total_score_displayed: float
 var total_score_target: int
 @export var level_score_label: Label
-var level_score: int
+var level_score_displayed: float
 var level_score_target: int
 
 @export var objective_display: Label
@@ -24,12 +24,22 @@ func _ready() -> void:
 	SignalBus.level_win_condition_met.connect(_on_level_win_conditon_met)
 	
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if current_level:
 		target_progress_value = current_level.completion
+		level_score_target = current_level.level_score
+		
 		
 		if current_level.timed:
 			time_left_display.text = str(snapped(current_level.completion_timer.time_left, 0.001))
+	
+	if main_handler:
+		total_score_target = main_handler.total_score
+	
+	level_score_displayed = lerp(level_score_displayed, float(level_score_target), .1)
+	level_score_label.text = str(snappedi(level_score_displayed, 1))
+	total_score_displayed = lerp(total_score_displayed, float(total_score_target), .1)
+	total_score_label.text = str(roundi(total_score_displayed))
 	
 	progress_display.value = lerpf(progress_display.value, target_progress_value, .1)
 
@@ -44,7 +54,7 @@ func _on_level_entered(p_level: Level) -> void:
 	else:
 		hide_timer()
 
-func _change_objective(p_win_condition: Level.WIN_CONDITION = -1):
+func _change_objective(p_win_condition: int = -1):
 	var win_condition
 	if p_win_condition == -1:
 		win_condition = current_level.win_condition
@@ -59,8 +69,8 @@ func _on_level_win_conditon_met(p_level: Level):
 	if current_level == p_level:
 		if current_level.timed and current_level.stop_timer_on_win:
 			hide_timer()
-		if target_progress_value >= 1:
-			_change_objective(Level.WIN_CONDITION.None)
+		
+		_change_objective(Level.WIN_CONDITION.None)
 		
 		
 func hide_timer():
