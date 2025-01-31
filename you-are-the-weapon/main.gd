566ef0ca1f2eb9acd37fb415ce84e12e3b846a3d
Mangochicken <97166753+Mangochicken13@users.noreply.github.com>
2025-01-31 15:51:20 +0800

    Simple starting the game features

--- you-are-the-weapon/main.gd
+++ you-are-the-weapon/main.gd
@@ -1,6 +1,21 @@
 extends Node
 class_name Main
 
+@export var menu_ui: Menu
+@export var player_ui: PlayerUI
+
 @export var player: Ball
 
 var game_started: bool
+
+func _ready() -> void:
+	player_ui.hide()
+	menu_ui.start_game.connect(_start_game)
+	
+	get_tree().paused = true
+
+func _start_game():
+	get_tree().paused = false
+	menu_ui.hide()
+	player_ui.show()
+	player.start_moving()
