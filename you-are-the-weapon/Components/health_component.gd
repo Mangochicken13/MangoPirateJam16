87db9fb769b9e396c7bb9c9062160e74b10aa6de
Mangochicken <97166753+Mangochicken13@users.noreply.github.com>
2025-02-05 18:13:22 +0800

    Start transition to composition over inheritance

---
+++ you-are-the-weapon/Components/health_component.gd
@@ -0,0 +1,15 @@
+extends Node3D
+class_name HealthComponent
+
+@export var MAX_HEALTH: float = 1.0
+var health: float
+
+signal health_depleted
+
+func _ready() -> void:
+	health = MAX_HEALTH
+
+func damage(damage: float) -> void:
+	health -= damage
+	if health <= 0:
+		health_depleted.emit()
