11ed3470acd01e4b0e05b9c78c6c2aaf4cced21a
Mangochicken <97166753+Mangochicken13@users.noreply.github.com>
2025-02-05 11:29:25 +0800

    Add debug menu

---
+++ you-are-the-weapon/UI/Debug/debug_ui.gd
@@ -0,0 +1,10 @@
+extends Control
+
+@export var frame_rate_label: Label
+@export var camera_spring_label: Label
+@export var player: Ball
+
+func _process(delta: float) -> void:
+	
+	frame_rate_label.text = str(Engine.get_frames_per_second())
+	camera_spring_label.text = "%.2f" % player.ball_cam.get_spring_length()
