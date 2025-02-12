d2644a760e4369eccac1cd97e5ad0fc061409ae8
Mangochicken <97166753+Mangochicken13@users.noreply.github.com>
2025-02-12 13:43:34 +0800

    Add collision layer enum to Utils autoload

--- you-are-the-weapon/utils.gd
+++ you-are-the-weapon/utils.gd
@@ -1,13 +1,20 @@
 extends Node
+# autoload Utils
 
-static func get_children_of_type(parent: Node, type: Variant) -> int:
+enum COLLISION_LAYERS {
+	Balls = 1 << 0, 			# 0001
+	Solid_Walls = 1 << 1, 		# 0010
+	Triggers = 1 << 2,			# 0100
+}
+
+static func get_children_of_type(p_parent: Node, p_type: Variant) -> int:
 	var num: int = 0
-	if !parent:
+	if !p_parent:
 		push_error("No node provided")
 		return 0
-	for node: Node in parent.get_children():
+	for node: Node in p_parent.get_children():
 		if node.get_child_count() > 0:
-			num += get_children_of_type(node, type)
-		if is_instance_of(node, type):
+			num += get_children_of_type(node, p_type)
+		if is_instance_of(node, p_type):
 			num += 1
 	return num
