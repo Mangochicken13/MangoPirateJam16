de4f50f7853cae49c1dda104ab45f1359b6e91b8
Mangochicken <97166753+Mangochicken13@users.noreply.github.com>
2025-02-22 21:48:09 +0800

    remove final traces of SignalBus

--- you-are-the-weapon/UI/win_screen.gd
+++ you-are-the-weapon/UI/win_screen.gd
@@ -4,9 +4,10 @@ class_name WinScreen
 @export var restart_button: Button
 @export var score_label: Label
 
+signal game_restart_requested
 
 func _ready() -> void:
 	restart_button.pressed.connect(request_restart)
 
-func request_restart():
-	SignalBus.restart_game.emit()
+func request_restart() -> void:
+	game_restart_requested.emit()
