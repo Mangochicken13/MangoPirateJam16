da85d71d595ba84c71d74423fb512386b84fc3be
Mangochicken <97166753+Mangochicken13@users.noreply.github.com>
2025-02-22 12:20:01 +0800

    Move primitive shape data to unique resource

--- you-are-the-weapon/Bricks/primitive_brick.gd
+++ you-are-the-weapon/Bricks/primitive_brick.gd
@@ -2,44 +2,18 @@
 extends BaseBrick
 class_name PrimitiveBrick
 
-enum PRIMITIVE_SHAPES {
-	Sphere, # Radius: float
-	Box, # Size: Vec3
-	Capsule, # Radius: float, Height: float
-	Cylinder, # Radius: float, Height: float
-}
-
-@export_group("Shape")
-@export var primitive_shape: PRIMITIVE_SHAPES = PRIMITIVE_SHAPES.Box:
-	set(shape):
-		primitive_shape = shape
-		_change_shape()
-		_update_shape()
-		notify_property_list_changed()
-
-## Affects Sphere, Cylinder, and Capsule shapes
-@export_range(0.001, 100, 0.001, "or_greater") var radius: float = 0.5:
-	set(new_radius):
-		radius = new_radius
-		if radius * 2 > height and primitive_shape == PRIMITIVE_SHAPES.Capsule:
-			height = radius * 2
-		_update_shape()
-## Affects Cylinder, and Capsule shapes
-@export_range(0.001, 100, 0.001, "or_greater") var height: float = 2.0:
-	set(new_height):
-		height = new_height
-		if height / 2 < radius and primitive_shape == PRIMITIVE_SHAPES.Capsule:
-			radius = height / 2
-		_update_shape()
-
-## Affects Box
-@export var size: Vector3 = Vector3.ONE:
-	set(new_size):
-		for i: int in range(3):
-			if new_size[i] < 0.0:
-				new_size[i] = 0.01
-		size = new_size
-		_update_shape()
+@export var shape: PrimitiveShape:
+	set(new_shape):
+		if shape:
+			if shape.shape_type_changed.is_connected(_change_shape):
+				shape.shape_type_changed.disconnect(_change_shape)
+			if shape.changed.is_connected(_update_shape):
+				shape.changed.disconnect(_update_shape)
+		
+		shape = new_shape
+		if shape != null:
+			shape.shape_type_changed.connect(_change_shape)
+			shape.changed.connect(_update_shape)
 
 @export_group("Editor Tools")
 @export_tool_button("Reset Collision Shape") var update_hitbox_shape_button : Callable = reset_collision_shape
@@ -47,36 +21,36 @@ enum PRIMITIVE_SHAPES {
 
 #region Editor Functions
 
-func _get_primitive_shape(p_type: PRIMITIVE_SHAPES) -> Shape3D:
+func _get_primitive_shape(p_type: PrimitiveShape.PRIMITIVE_SHAPES) -> Shape3D:
 	match p_type:
-		PRIMITIVE_SHAPES.Sphere:
+		PrimitiveShape.PRIMITIVE_SHAPES.Sphere:
 			return SphereShape3D.new()
-		PRIMITIVE_SHAPES.Box:
+		PrimitiveShape.PRIMITIVE_SHAPES.Box:
 			return BoxShape3D.new()
-		PRIMITIVE_SHAPES.Capsule:
+		PrimitiveShape.PRIMITIVE_SHAPES.Capsule:
 			return CapsuleShape3D.new()
-		PRIMITIVE_SHAPES.Cylinder:
+		PrimitiveShape.PRIMITIVE_SHAPES.Cylinder:
 			return CylinderShape3D.new()
 		var shape:
 			push_warning("invalid shape index: %s" % shape)
 			return null
 
-func _get_primitive_mesh(p_type: PRIMITIVE_SHAPES) -> Mesh:
+func _get_primitive_mesh(p_type: PrimitiveShape.PRIMITIVE_SHAPES) -> Mesh:
 	# Reducing the faces on the round shapes should save rendering time?
 	# Boxes should only have 12 polygons in most circumstances
 	match p_type:
-		PRIMITIVE_SHAPES.Sphere:
+		PrimitiveShape.PRIMITIVE_SHAPES.Sphere:
 			var sphere_mesh: SphereMesh = SphereMesh.new()
 			sphere_mesh.rings = 16
 			sphere_mesh.radial_segments = 32
 			return sphere_mesh
-		PRIMITIVE_SHAPES.Box:
+		PrimitiveShape.PRIMITIVE_SHAPES.Box:
 			return BoxMesh.new()
-		PRIMITIVE_SHAPES.Capsule:
+		PrimitiveShape.PRIMITIVE_SHAPES.Capsule:
 			var capsule_mesh: CapsuleMesh = CapsuleMesh.new()
 			capsule_mesh.radial_segments = 32
 			return capsule_mesh
-		PRIMITIVE_SHAPES.Cylinder:
+		PrimitiveShape.PRIMITIVE_SHAPES.Cylinder:
 			var cylinder_mesh: CylinderMesh = CylinderMesh.new()
 			cylinder_mesh.radial_segments = 32
 			return cylinder_mesh
@@ -124,7 +98,7 @@ func reset_collision_shape() -> bool:
 				var child = hitbox_component.get_child(i)
 				
 				if child is CollisionShape3D:
-					var new_shape: Shape3D = _get_primitive_shape(primitive_shape)
+					var new_shape: Shape3D = _get_primitive_shape(shape.primitive_shape)
 					child.shape = new_shape
 					if i + 1 < children:
 						print("More children than expected in %s" % hitbox_component.name)
@@ -134,22 +108,25 @@ func reset_collision_shape() -> bool:
 	return false
 
 func _change_shape() -> void:
-	var shape: Shape3D = _get_primitive_shape(primitive_shape)
-	var mesh: Mesh = _get_primitive_mesh(primitive_shape)
-	var outline_mesh: Mesh = mesh.duplicate() # has to be a different mesh instance, otherwise changes are synced
-	if not shape or not mesh:
-		push_error("Invalid shape index passed")
-		return
 	if not hitbox_component:
 		return
 	
+	var new_shape: Shape3D = _get_primitive_shape(shape.primitive_shape)
+	var new_mesh: Mesh = _get_primitive_mesh(shape.primitive_shape)
+	
+	if not new_shape or not new_mesh:
+		push_error("Invalid shape index passed")
+		return
+	
+	var new_outline_mesh: Mesh = new_mesh.duplicate() # has to be a different mesh instance, otherwise changes are synced
+	
 	
 	if hitbox_component.is_primitive and hitbox_component.primitive_collision_shape:
-		hitbox_component.primitive_collision_shape.shape = shape
+		hitbox_component.primitive_collision_shape.shape = new_shape
 		if mesh_component:
-			mesh_component.mesh = mesh
+			mesh_component.mesh = new_mesh
 		if mesh_component.outline_mesh:
-			mesh_component.outline_mesh.mesh = outline_mesh
+			mesh_component.outline_mesh.mesh = new_outline_mesh
 	else: 
 		# Important in case these settings get toggled off so i know why nothing is happening
 		push_warning("Hitbox \"%s\" is not marked as primitive " % hitbox_component.name)
@@ -178,56 +155,56 @@ func _update_shape() -> void:
 	if hitbox_component.primitive_collision_shape:
 		collision_shape = hitbox_component.primitive_collision_shape.shape
 		
-	match primitive_shape:
-		PRIMITIVE_SHAPES.Sphere:
+	match shape.primitive_shape:
+		PrimitiveShape.PRIMITIVE_SHAPES.Sphere:
 			if mesh is SphereMesh:
-				mesh.radius = radius
-				mesh.height = radius * 2
+				mesh.radius = shape.radius
+				mesh.height = shape.radius * 2
 				
 			if outline_mesh is SphereMesh:
-				outline_mesh.radius = radius + 0.1
-				outline_mesh.height = radius * 2 + 0.2
+				outline_mesh.radius = shape.radius + 0.1
+				outline_mesh.height = shape.radius * 2 + 0.2
 				
 			if collision_shape is SphereShape3D:
-				collision_shape.radius = radius
+				collision_shape.radius = shape.radius
 				
-		PRIMITIVE_SHAPES.Box:
+		PrimitiveShape.PRIMITIVE_SHAPES.Box:
 			if mesh is BoxMesh:
-				mesh.size = size
+				mesh.size = shape.size
 				
 			if outline_mesh is BoxMesh:
-				outline_mesh.size = size + Vector3(0.2, 0.2, 0.2)
+				outline_mesh.size = shape.size + Vector3(0.2, 0.2, 0.2)
 				
 			if collision_shape is BoxShape3D:
-				collision_shape.size = size
+				collision_shape.size = shape.size
 				
-		PRIMITIVE_SHAPES.Capsule:
+		PrimitiveShape.PRIMITIVE_SHAPES.Capsule:
 			if mesh is CapsuleMesh:
-				mesh.height = height
-				mesh.radius = radius
+				mesh.height = shape.height
+				mesh.radius = shape.radius
 				
 			if outline_mesh is CapsuleMesh:
-				outline_mesh.height = height + 0.2
-				outline_mesh.radius = radius + 0.1
+				outline_mesh.height = shape.height + 0.2
+				outline_mesh.radius = shape.radius + 0.1
 				
 			if collision_shape is CapsuleShape3D:
-				collision_shape.height = height
-				collision_shape.radius = radius
+				collision_shape.height = shape.height
+				collision_shape.radius = shape.radius
 				
-		PRIMITIVE_SHAPES.Cylinder:
+		PrimitiveShape.PRIMITIVE_SHAPES.Cylinder:
 			if mesh is CylinderMesh:
-				mesh.height = height
-				mesh.top_radius = radius
-				mesh.bottom_radius = radius
+				mesh.height = shape.height
+				mesh.top_radius = shape.radius
+				mesh.bottom_radius = shape.radius
 				
 			if outline_mesh is CylinderMesh:
-				outline_mesh.height = height + 0.2
-				outline_mesh.top_radius = radius + 0.1
-				outline_mesh.bottom_radius = radius + 0.1
+				outline_mesh.height = shape.height + 0.2
+				outline_mesh.top_radius = shape.radius + 0.1
+				outline_mesh.bottom_radius = shape.radius + 0.1
 				
 			if collision_shape is CylinderShape3D:
-				collision_shape.height = height
-				collision_shape.radius = radius
+				collision_shape.height = shape.height
+				collision_shape.radius = shape.radius
 				
 	
 	if health_component:
@@ -254,35 +231,3 @@ func _min_outline_dither_dist(p_length: float) -> float:
 func _max_outline_dither_dist(p_length: float) -> float:
 	var increase: float = minf(7.0, p_length * 0.5) + 1
 	return p_length + increase
-
-#endregion
-
-func _validate_property(p_property: Dictionary) -> void:
-	match primitive_shape:
-		PRIMITIVE_SHAPES.Sphere:
-			match p_property.name:
-				#"radius". \
-				"height", \
-				"size":
-					p_property.usage = PROPERTY_USAGE_NO_EDITOR
-		
-		PRIMITIVE_SHAPES.Box:
-			match p_property.name:
-				"radius", \
-				"height":
-				#"size". \
-					p_property.usage = PROPERTY_USAGE_NO_EDITOR
-		
-		PRIMITIVE_SHAPES.Capsule:
-			match p_property.name:
-				#"radius", \
-				#"height". \
-				"size":
-					p_property.usage = PROPERTY_USAGE_NO_EDITOR
-		
-		PRIMITIVE_SHAPES.Cylinder:
-			match p_property.name:
-				#"radius", \
-				#"height", \
-				"size":
-					p_property.usage = PROPERTY_USAGE_NO_EDITOR
